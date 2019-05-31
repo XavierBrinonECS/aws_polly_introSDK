02484ddd1dd3da5e7a341bd1fb20b17b287a13b1
Xavier Brinon <xavier.brinon@ecs.co.uk>
2019-05-31 10:18:30 +0100

    Add some css to make less ugly.

--- my-little-poney/src/App.tsx
+++ my-little-poney/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { PollyApp } from './components/PollyApp';
@@ -11,15 +10,6 @@ const App: React.FC = () => {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
       <main id='pollyApp'>
         <PollyApp />
       </main>
